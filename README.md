e6a391e91ecc77aca873b84977a00fe623c6ae0d
freewind <nowindlee@gmail.com>
2019-03-31 16:13:35 +0800

    fix

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ TypeScript React Children in Function Component Demo
 
 关键点就在于，只要自己定义一个`children`就行了。
 
+注：children的类型声明为`any`比`ReactNode`好，因为children可能不止ReactNode这一种类型
+
 ```
 npm install
 npm run demo
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,5 +1,5 @@
-import React, {ReactNode} from "react";
+import React from "react";
 
-export default function Panel({children}: { children: ReactNode }) {
+export default function Panel({children}: { children: any }) {
   return <div style={{border: '1px solid red'}}>{children}</div>
 };
